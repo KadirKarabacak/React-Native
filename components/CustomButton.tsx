b9e3df776bf29c4f317174dd82197db463f0af8c
KadirKarabacak <kadirht@hotmail.com>
2024-08-22 16:05:49 +0300

    feat: create custom button

---
+++ components/CustomButton.tsx
@@ -0,0 +1,50 @@
+import { Text, TouchableOpacity } from "react-native";
+import React from "react";
+import { pSemibold } from "@/constants/fonts";
+
+interface CustomButtonProps {
+    title: string;
+    handlePress: () => void;
+    containerStyles: object;
+    textStyles: object;
+    isLoading: boolean;
+}
+
+const CustomButton = ({
+    title,
+    handlePress,
+    containerStyles,
+    textStyles,
+    isLoading,
+}: CustomButtonProps) => {
+    return (
+        <TouchableOpacity
+            onPress={handlePress}
+            activeOpacity={0.7}
+            style={{
+                backgroundColor: "#FF9C01",
+                borderRadius: 50,
+                minHeight: 62,
+                justifyContent: "center",
+                alignItems: "center",
+                opacity: isLoading ? 0.5 : 1,
+                ...containerStyles,
+            }}
+            disabled={isLoading}
+        >
+            <Text
+                style={{
+                    color: "#161622",
+                    fontFamily: pSemibold,
+                    fontSize: 18,
+                    lineHeight: 28,
+                    ...textStyles,
+                }}
+            >
+                {title}
+            </Text>
+        </TouchableOpacity>
+    );
+};
+
+export default CustomButton;
