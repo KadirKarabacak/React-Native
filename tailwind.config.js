56cea44c10ab7dcae14feb8bb1aa961da25ea2cd
KadirKarabacak <kadirht@hotmail.com>
2024-08-23 00:53:53 +0300

    chore:

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 module.exports = {
     content: [
         "./app/**/*.{js,jsx,ts,tsx}",
+        "./screens/**/*.{js,jsx,ts,tsx}",
         "./components/**/*.{js,jsx,ts,tsx}",
     ],
     theme: {
