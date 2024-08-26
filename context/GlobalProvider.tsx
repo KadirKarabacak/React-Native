5d32a0c448be1a56ef5869ab60d57c2e8acda994
KadirKarabacak <kadirht@hotmail.com>
2024-08-26 11:57:21 +0300

    feat: create global context to provide user

---
+++ context/GlobalProvider.tsx
@@ -0,0 +1,51 @@
+import { getCurrentUser } from "@/lib/appwrite";
+import React, {
+    createContext,
+    useContext,
+    useState,
+    ReactNode,
+    useEffect,
+} from "react";
+
+// 1. GlobalContext oluşturma
+const GlobalContext = createContext<any>(null);
+
+// 2. useGlobalContext hook'u ile context'i kullanıma sunun
+export const useGlobalContext = () => useContext(GlobalContext);
+
+// 3. GlobalProvider bileşenini oluşturun ve value prop'unu sağlayın
+interface GlobalProviderProps {
+    children: ReactNode;
+}
+
+const GlobalProvider = ({ children }: GlobalProviderProps) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [user, setUser] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        getCurrentUser()
+            .then(res => {
+                setIsLoading(true);
+                if (res) {
+                    setIsLoggedIn(true);
+                    setUser(res);
+                } else {
+                    setIsLoggedIn(false);
+                    setUser(null);
+                }
+            })
+            .catch(error => console.log(error))
+            .finally(() => setIsLoading(false));
+    }, []);
+
+    return (
+        <GlobalContext.Provider
+            value={{ isLoggedIn, setIsLoggedIn, user, setUser, isLoading }}
+        >
+            {children}
+        </GlobalContext.Provider>
+    );
+};
+
+export default GlobalProvider;
