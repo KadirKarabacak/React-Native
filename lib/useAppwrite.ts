be4af8dc5739a842542acab77fdfa1142d920b65
KadirKarabacak <kadirht@hotmail.com>
2024-08-27 23:14:45 +0300

    feat: create reusable fetchdata hook to handle loading & refetch states

---
+++ lib/useAppwrite.ts
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import { Alert } from "react-native";
+
+const useAppwrite = fn => {
+    const [data, setData] = useState<any>([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const fetchData = async () => {
+        setIsLoading(true);
+        try {
+            const response = await fn();
+            setData(response);
+        } catch (error: any) {
+            Alert.alert("Error", error.message);
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    // Be careful u use fetch data as callback otherwise it cause infine loop
+    const refetch = () => fetchData();
+
+    return { data, isLoading, refetch };
+};
+
+export default useAppwrite;
