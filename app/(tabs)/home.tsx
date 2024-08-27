b06446e207c7611ad3fd95fd4df00fec050adabe
KadirKarabacak <kadirht@hotmail.com>
2024-08-27 23:12:16 +0300

    feat: update homepage with real data flat lists & control refresh list

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,22 +1,38 @@
-import { View, Text, FlatList } from "react-native";
-import React from "react";
+import EmptyState from "@/components/EmptyState";
+import SearchInput from "@/components/SearchInput";
+import Trending from "@/components/Trending";
+import VideoCard from "@/components/VideoCard";
+import { images } from "@/constants";
+import { pMedium, pRegular, pSemibold } from "@/constants/fonts";
+import { getAllPosts, getLatestPosts } from "@/lib/appwrite";
+import useAppwrite from "@/lib/useAppwrite";
+import React, { useState } from "react";
+import { FlatList, Image, RefreshControl, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { pMedium, pSemibold } from "@/constants/fonts";
 
 const Home = () => {
+    const { data: posts, refetch } = useAppwrite(getAllPosts);
+    const { data: latestPosts } = useAppwrite(getLatestPosts);
+    const [refreshing, setRefreshing] = useState(false);
+
+    // Create for <RefreshControl /> Allow user to pull screen down to refresh
+    const onRefresh = async () => {
+        setRefreshing(true);
+
+        // re-call videos -> if any new video appeard
+        await refetch();
+
+        setRefreshing(false);
+    };
+
     return (
         <SafeAreaView style={{ backgroundColor: "#161622", height: "100%" }}>
             <FlatList
-                // Actual Data
-                data={[{ id: 1 }, { id: 2 }, { id: 3 }]}
+                data={posts}
                 // Key prop
-                keyExtractor={item => String(item.id)}
+                keyExtractor={item => String(item.thumbnail)}
                 // Rendering item
-                renderItem={({ item }) => (
-                    <Text style={{ fontSize: 50, color: "white" }}>
-                        {item.id}
-                    </Text>
-                )}
+                renderItem={({ item }) => <VideoCard video={item} />}
                 // Header component of list
                 ListHeaderComponent={() => (
                     <View
@@ -29,35 +45,85 @@ const Home = () => {
                     >
                         <View
                             style={{
-                                justifyContent: "space-between",
                                 alignItems: "start",
-                                // flexDirection: "row",
                                 marginBottom: 24,
+                                flexDirection: "row",
+                                justifyContent: "space-between",
+                            }}
+                        >
+                            <View>
+                                <Text
+                                    style={{
+                                        fontFamily: pMedium,
+                                        fontSize: 14,
+                                        lineHeight: 16,
+                                        color: "rgb(243 244 246)",
+                                    }}
+                                >
+                                    Welcome Back
+                                </Text>
+                                <Text
+                                    style={{
+                                        fontFamily: pSemibold,
+                                        fontSize: 24,
+                                        lineHeight: 32,
+                                        color: "white",
+                                    }}
+                                >
+                                    Ex Reminder
+                                </Text>
+                            </View>
+
+                            <View style={{ marginTop: 10 }}>
+                                <Image
+                                    style={{ width: 36, height: 40 }}
+                                    source={images.logoSmall}
+                                    resizeMode="contain"
+                                />
+                            </View>
+                        </View>
+
+                        <SearchInput
+                            value="null"
+                            handleChangeText={() => {}}
+                            placeholder="Search for a video topic"
+                        />
+                        <View
+                            style={{
+                                width: "100%",
+                                flex: 1,
+                                paddingTop: 15,
+                                paddingBottom: 10,
                             }}
                         >
                             <Text
                                 style={{
-                                    fontFamily: pMedium,
-                                    fontSize: 14,
-                                    lineHeight: 16,
+                                    fontFamily: pRegular,
                                     color: "rgb(243 244 246)",
+                                    fontSize: 14,
+                                    lineHeight: 28,
                                 }}
                             >
-                                Welcome Back
+                                Latest Videos
                             </Text>
-                            <Text
-                                style={{
-                                    fontFamily: pSemibold,
-                                    fontSize: 24,
-                                    lineHeight: 32,
-                                    color: "white",
-                                }}
-                            >
-                                Ex Reminder Test
-                            </Text>
+                            <Trending posts={latestPosts ?? []} />
                         </View>
                     </View>
                 )}
+                // Works when list is empty
+                ListEmptyComponent={() => (
+                    <EmptyState
+                        title="No Videos Found"
+                        subtitle="Be the first one to upload a video"
+                    />
+                )}
+                // Allow user to refresh screen by pulling down
+                refreshControl={
+                    <RefreshControl
+                        refreshing={refreshing}
+                        onRefresh={onRefresh}
+                    />
+                }
             />
         </SafeAreaView>
     );
